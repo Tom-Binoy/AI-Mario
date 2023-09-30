03062f7be809bcbefd0cc8e07a088e2503fbc953
Tom-Binoy <111070900+Tom-Binoy@users.noreply.github.com>
2023-09-30 12:48:15 +0530

    Add files via upload

--- characters_environment.js
+++ characters_environment.js
@@ -57,12 +57,12 @@ nosey = "";
 gamestatus = "";
 
 function startgame(){
-  gamestatus = true;
+  gamestatus = "true";
   document.getElementById("status").innerHTML = "Game is Loading!!";
 }
 
 function game(){
-  //console.log("Nose X : "+nosex+" Nose Y : "+nosey)
+  console.log("Nose X : "+nosex+" Nose Y : "+nosey)
   instializeInDraw();
   moveEnvironment(mario);
   drawSprites();
@@ -75,7 +75,7 @@ function game(){
     fill(255, 255, 255);
     textSize(40);
     textAlign(CENTER);
-    text("Press Any Arrow Keys to Start and Play ", gameConfig.screenX/2, gameConfig.screenY/2);
+    text('Click "Start" and move your nose to play.', gameConfig.screenX/2, gameConfig.screenY/2);
     textSize(40);
 
     stroke(255);
@@ -123,7 +123,7 @@ function game(){
 
 // change game status if any key is pressed
 function changeGameStatud(character){
-  if((keyDown(control.up) ||keyDown(control.left)||keyDown(control.right) )&& gameConfig.status==="start") {
+  if(gamestatus==="true" && nosex!="" && gameConfig.status==="start") {
     world_start.play();
     initializeCharacterStatus(mario);
     gameConfig.status= "play";
@@ -287,13 +287,13 @@ function autoControl(character){
 function manualControl(character){
   
   if(character.live){
-    if(keyDown(control.left)){
+    if(nosex<=399 ){
       character.velocity.x-=gameConfig.moveSpeed;
       character.changeAnimation('move');
       character.mirrorX(-1);
     }
 
-    if(keyDown(control.right)){
+    if(nosex>=401){
       character.velocity.x+=gameConfig.moveSpeed;
       character.changeAnimation('move');
       character.mirrorX(1);
@@ -308,7 +308,7 @@ function manualControl(character){
 
 /* Movements of character */
 function jumping(character){
-	if( (keyWentDown(control.up)&&character.live) || (touchIsDown&&character.live) ){
+	if( (nosey<201 &&character.live) || (touchIsDown&&character.live) ){
 		character.velocity.y+=gameConfig.jump;
 	}
 }
